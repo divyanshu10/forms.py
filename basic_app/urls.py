5497ab9c756174406a6a98e2bd57dca729d76844
Divyanshu <divyanshugairola@gmail.com>
2018-03-23 01:37:50 +0530

    form prob

---
+++ basic_app/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from basic_app import views
+app_name='basic_app'
+urlpatterns = [
+    path('register',views.register,name='register'),
+    # path('',views.index,name=''),
+    # path('',views.index,name=''),
+   
+]
